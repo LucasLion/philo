1ee2ac2229d5a3ea6ca7e674a7d5331bebac34a2
Lucas Lion <llion@k0r2p1.42mulhouse.fr>
2023-04-12 16:42:58 +0200

    SEGV OK

--- src/philo.c
+++ src/philo.c
@@ -6,11 +6,18 @@
 /*   By: llion <llion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by llion             #+#    #+#             */
-/*   Updated: 2023/04/12 16:35:09 by llion            ###   ########.fr       */
+/*   Updated: 2023/04/12 16:42:34 by llion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
+
+void	create_thread(t_philo *p)
+{
+	p->thread = malloc(sizeof(pthread_t));
+	pthread_create(p->thread, NULL, &eating, p);
+	printf("coucou\n");
+}
 
 int main(int argc, char **argv)
 {
@@ -21,6 +28,10 @@ int main(int argc, char **argv)
 	if (!initialization(params, argc, argv))
 		return (-1);
 	table = create_table(params);
-	pthread_create(table->philos[0]->thread, NULL, &eating, NULL);
+	int i = 0;
+	while (table->philos[i])
+	{
+		create_thread(table->philos[i]);
+	}
 	return (0);
 }
